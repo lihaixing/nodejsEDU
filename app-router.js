94da047fd02e5fe5ec01a7884989e045c315f9d1
李海兴 <lihaixing@xylink.com>
2018-12-09 14:26:54 +0800

    router post controller

---
+++ app-router.js
@@ -0,0 +1,50 @@
+const Koa = require('koa');
+// 注意require('koa-router')返回的是函数:
+const router = require('koa-router')();
+const bodyParser = require('koa-bodyparser');
+
+const app = new Koa();
+// 有了这个就可以进行post请求了
+app.use(bodyParser());
+// log request URL:
+app.use(async (ctx, next) => {
+    console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
+    await next();
+});
+
+// add url-route:
+router.get('/hello/:name', async (ctx, next) => {
+    var name = ctx.params.name;
+    console.log(JSON.stringify(ctx, null, '   '))
+    ctx.response.body = `<h1>Hello, ${name}!</h1>`
+});
+
+router.get('/', async (ctx, next) => {
+    ctx.response.body = `
+        <h1>Index</h1>
+        <form action="/signin" method="post">
+            <p>Name: <input name="name" value="koa"></p>
+            <p>Password: <input name="password" type="password"></p>
+            <p><input type="submit" value="Submit"></p>
+        </form>`
+});
+
+router.post('/signin', async (ctx, next) => {
+    console.log(ctx)
+    var name = ctx.request.body.name || '',
+        password = ctx.request.body.password || '';
+    console.log(`signin with name: ${name}, password: ${password}`);
+    if (name === 'koa' && password === '12345') {
+        ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
+    } else {
+        ctx.response.body = `<h1>Login failed!</h1>
+        <p><a href="/">Try again</a></p>`;
+    }
+});
+
+// add router middleware:
+app.use(router.routes());
+
+// 在端口3000监听:
+app.listen(3000);
+console.log('app started at port 3000...');
--- app.js
+++ app.js
@@ -5,8 +5,23 @@ const Koa = require('koa');
 const app = new Koa();
 
 // 对于任何请求，app将调用该异步函数处理请求：
+// 最后注意ctx对象有一些简写的方法，例如ctx.url相当于ctx.request.url，ctx.type相当于ctx.response.type
+app.use(async (ctx, next) => {
+    console.log(`${ctx.request.method} ${ctx.request.url}`); // 打印URL
+    await next(); // 调用下一个middleware
+});
+
+app.use(async (ctx, next) => {
+    const start = new Date().getTime(); // 当前时间
+    await next(); // 调用下一个middleware
+    console.log(1)
+    const ms = new Date().getTime() - start; // 耗费时间
+    console.log(`Time: ${ms}ms`); // 打印耗费时间
+});
+
 app.use(async (ctx, next) => {
     await next();
+    console.log(2)
     ctx.response.type = 'text/html';
     ctx.response.body = '<h1>Hello, koa2!</h1>';
 });
